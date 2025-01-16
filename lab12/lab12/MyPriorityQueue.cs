abb95c1fa4e44a52574e14b9f6a394894cab1f9b
xz-aha-haha <xz.github@gmail.com>
2025-01-16 20:33:34 +0300

    lab 12
    Задача 12.
    С клавиатуры вводится количество шагов добавления заявок в приоритетную очередь 𝑁.
    На каждом шаге производятся следующие действия: генерируются и добавляются в
    очередь от 1 до 10 заявок (конкретное число заявок выбирается случайно). Каждая заявка
    содержит следующую информацию: приоритет (случайное целое число от 1 до 5), номер
    заявки (заявки считаются начиная с 1, нумерация сквозная на всех шагах), номер шага на
    котором заявка поступила в систему. После добавления заявок, заявка с наибольшим
    приоритетом удаляется.
    После завершения N шагов генерации заявок, шаги продолжаются без генерации (только
    удаление) до тех пор, пока очередь не станет пуста.
    Необходимо подсчитать максимальное время ожидания заявки в системе и вывести всю
    информацию о заявке, которая ожидала максимальное время.
    Кроме того, следует сохранять информацию о каждом добавлении заявки в очередь и
    удалении заявки в очередь в файл log.txt. Запись в файле имеет следующую структуру:
    ADD/REMOVE НомерЗаявки Приоритет НомерШага
    Для хранения и обработки данных использовать собственную реализацию очереди с
    приоритетами MyPriorityQueue.

---
+++ lab12/lab12/MyPriorityQueue.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyPriorityQueue
+{
+    public class MyPriorityQueue<T> where T : IComparable<T>
+    {
+        T[] queue;
+        int size;
+        Comparer<T> comparer;
+
+        public MyPriorityQueue()
+        {
+            queue = new T[11];
+            size = 0;
+        }
+        public MyPriorityQueue(T[] a)
+        {
+            queue = new T[a.Length];
+            size = 0;
+            for (int i = 0; i < a.Length; i++) Add(a[i]);
+        }
+        public MyPriorityQueue(int initialCapacity)
+        {
+            queue = new T[initialCapacity];
+            size = initialCapacity;
+        }
+        public MyPriorityQueue(int initialCapacity, Comparer<T> comparer)
+        {
+            queue = new T[initialCapacity];
+            size = initialCapacity;
+            this.comparer = comparer;
+        }
+        public MyPriorityQueue(MyPriorityQueue<T> c)
+        {
+            queue = new T[c.size];
+            size = c.size;
+            for (int i = 0; i < size; i++) queue[i] = c.queue[i];
+        }
+
+        private void Swap(int index_1, int index_2)
+        {
+            T tmp = queue[index_1];
+            queue[index_1] = queue[index_2];
+            queue[index_2] = tmp;
+        }
+        private void Heapify(int index, Comparer<T> comparer)
+        {
+            while (index > 0)
+            {
+                int parentIndex = (index - 1) / 2;
+                if (queue[index].CompareTo(queue[parentIndex]) <= 0) break;
+                Swap(index, parentIndex);
+                index = parentIndex;
+            }
+        }
+        private void Resize()
+        {
+            int newCapacity = queue.Length < 64 ? queue.Length + 2 : queue.Length * 3 / 2;
+            Array.Resize(ref queue, newCapacity);
+        }
+        private int FindIndex(object o)
+        {
+            if (Contains(o))
+            {
+                for (int i = 0; i < queue.Length; i++)
+                {
+                    if (o.Equals(queue[i])) return i;
+                }
+            }
+            return -1;
+        }
+
+        public void Add(T e)
+        {
+            if (size == queue.Length) Resize();
+            queue[size++] = e;
+            Heapify(size - 1, comparer);
+        }
+        public void AddAll(T[] a)
+        {
+            for (int i = 0; i < a.Length; i++) Add(a[i]);
+        }
+        public void Clear()
+        {
+            Array.Clear(queue, 0, size);
+            size = 0;
+        }
+        public bool Contains(object o)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                if (Equals(o, queue[i])) return true;
+            }
+            return false;
+        }
+        public bool ContainsAll(T[] a)
+        {
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (!Contains(a[i])) return false;
+            }
+            return true;
+        }
+        public bool IsEmpty() => size == 0;
+        public void Remove(object o)
+        {
+            if (Contains(o))
+            {
+                int k = FindIndex(o);
+                T[] array = new T[size - 1];
+                for (int i = 0; i < k; i++) array[i] = queue[i];
+                for (int i = k + 1; i < size; i++) array[i - 1] = queue[i];
+                queue = array;
+                size--;
+                Heapify(size - 1, comparer);
+            }
+        }
+        public void RemoveAll(T[] a)
+        {
+            for (int i = 0; i < a.Length; i++) Remove(a[i]);
+        }
+        public void RetainAll(T[] a)
+        {
+            if (a.Length > queue.Length) Array.Resize(ref queue, a.Length);
+            for (int i = 0; i < a.Length; i++) queue[i] = a[i];
+            size = a.Length;
+        }
+        public int Size() => size;
+        public T[] ToArray()
+        {
+            T[] array = new T[size];
+            for (int i = 0; i < size; i++) array[i] = queue[i];
+            return array;
+        }
+        public void ToArray(T[] a)
+        {
+            if (a == null) a = new T[size];
+            else if (a.Length < size) Array.Resize<T>(ref a, size);
+            for (int i = 0; i < a.Length; i++) a[i] = queue[i];
+        }
+        public T Element() => queue[0];
+        public bool Offer(T obj)
+        {
+            Add(obj);
+            if (Contains(obj)) return true;
+            return false;
+        }
+        public T Peek() => size == 0 ? default(T) : queue[0];
+        public T Poll()
+        {
+            if (size == 0) return default(T);
+            T obj = queue[0];
+            Remove(obj);
+            return obj;
+        }
+        public void Print()
+        {
+            Console.Write("Current queue is: ");
+            if (size == 0)
+            {
+                Console.WriteLine("empty");
+                return;
+            }
+            for (int i = 0; i < size; i++)
+            {
+                Console.Write(queue[i] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ lab12/lab12/Program.cs
@@ -0,0 +1,66 @@
+﻿using MyPriorityQueue;
+using System.Diagnostics;
+
+class Program
+{
+    public class Request : IComparable<Request>
+    {
+        public int number { get; set; }
+        public int priority { get; set; }
+        public int step { get; set; }
+        public Request(int priority, int number, int step)
+        {
+            this.number = number;
+            this.priority = priority;
+            this.step = step;
+        }
+        public int CompareTo(Request other)
+        {
+            if (priority != other.priority) return priority.CompareTo(other.priority);
+            else return number.CompareTo(other.number);
+        }
+    }
+    static void Main(string[] args)
+    {
+        string path = "log.txt";
+        MyPriorityQueue<Request> queue = new MyPriorityQueue<Request>();
+
+        Console.WriteLine("Amount of steps: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+        int k = 0;
+
+
+        Stopwatch stopwatch = new Stopwatch();
+        stopwatch.Start();
+        StreamWriter sw = new StreamWriter(path);
+
+        for (int i = 0; i < n; i++)
+        {
+            Random rnd = new Random();
+            int amountOfRequests = rnd.Next(1, 11);
+
+            for (int j = 0; j < amountOfRequests; j++)
+            {
+                int priority = rnd.Next(1, 6);
+                Request request = new Request(priority, j, i);
+                queue.Add(request);
+                sw.WriteLine($"ADD: {request.number} {request.priority} {request.step}");
+                k++;
+            }
+            Request tmp = queue.Peek();
+            sw.WriteLine($"REMOVE: {tmp.number} {tmp.priority} {tmp.step}");
+            queue.Remove(tmp);
+        }
+
+
+        for (int i = 0; i < (k-n); i++)
+        {
+            Request tmp = queue.Peek();
+            sw.WriteLine($"REMOVE: {tmp.number} {tmp.priority} {tmp.step}");
+            queue.Remove(tmp);
+        }
+        stopwatch.Stop();
+        sw.WriteLine($"Время выполнения: {stopwatch.Elapsed}");
+        sw.Close();
+    }
+}
